e2f90f26eef0e512002151d363c0ac7ed76563ee
Dewwlo <a.bjrnlund@gmail.com>
2017-11-28 21:11:44 +0100

    Add: project files.
    d

---
+++ ClientSocket.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Övningstenta
+{
+    public class ClientSocket
+    {
+        private readonly TcpClient _client = new TcpClient();
+        private NetworkStream _stream;
+        private readonly IPEndPoint _endPoint;
+        private Task _activeGame;
+        private static CancellationTokenSource _tokenSource;
+        public CancellationToken _ct;
+
+        public ClientSocket(string address, string port, out bool success)
+        {
+            _endPoint = RemoteEndPoint(address, port);
+            success = Connect();
+        }
+
+        private bool Connect()
+        {
+            try
+            {
+                _client.Connect(_endPoint);
+                _stream = _client.GetStream();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private IPEndPoint RemoteEndPoint(string address, string port)
+        {
+            return new IPEndPoint(IPAddress.Parse(address), Int32.Parse(port));
+        }
+
+        public void ClientSend(Command cmd)
+        {
+            var bytesToSend = Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(cmd));
+            _stream.Write(bytesToSend, 0, bytesToSend.Length);
+            _stream.Flush();
+            if (cmd.CommandTerm == "LEAVE")
+                _tokenSource.Cancel();
+                
+        }
+
+        public void ClientRecieve()
+        {
+            while (true)
+            {
+                if (_ct.IsCancellationRequested)
+                    _ct.ThrowIfCancellationRequested();
+
+                var data = new byte[1024];
+                var recv = _stream.Read(data, 0, data.Length);
+                var temp = Encoding.ASCII.GetString(data, 0, recv);
+                if (ValidateJson(temp))
+                {
+                    
+                }
+            }
+        }
+
+        public List<int> ClientRecieveGames()
+        {
+            var data = new byte[1024];
+            var recv = _stream.Read(data, 0, data.Length);
+            var games = Encoding.ASCII.GetString(data, 0, recv);
+            return !ValidateJson(games) ? new List<int>() : JsonConvert.DeserializeObject<List<int>>(games);
+        }
+
+        public int ClientRecieveGameId()
+        {
+            var data = new byte[1024];
+            var recv = _stream.Read(data, 0, data.Length);
+            var game = Encoding.ASCII.GetString(data, 0, recv);
+            var gameId = !ValidateJson(game) ? 0 : JsonConvert.DeserializeObject<int>(game);
+            if (gameId != 0)
+            {
+                CreateTask();
+                _activeGame.Start();
+            }
+
+            return gameId;
+        }
+
+        public bool JoinGameSucceeded()
+        {
+            var data = new byte[1024];
+            var recv = _stream.Read(data, 0, data.Length);
+            var game = Encoding.ASCII.GetString(data, 0, recv);
+            var succeeded = ValidateJson(game) && JsonConvert.DeserializeObject<bool>(game);
+            if (succeeded)
+            {
+                CreateTask();
+                _activeGame.Start();
+            }
+
+            return succeeded;
+        }
+
+        private void CreateTask()
+        {
+            _tokenSource = new CancellationTokenSource();
+            _ct = _tokenSource.Token;
+
+            _activeGame = new Task(() =>
+            {
+                _ct.ThrowIfCancellationRequested();
+                ClientRecieve();
+            }, _tokenSource.Token);
+
+        }
+
+        private static bool ValidateJson(string json)
+        {
+            try
+            {
+                var obj = JsonConvert.DeserializeObject<dynamic>(json);
+                return true;
+            }
+            catch // not valid
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Övningstenta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ClientSocket _clientSocket;
+        private List<int> _games = new List<int>();
+        private int _multiplayerGameId;
+        private string _gameMode = "";
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int Counter { get; set; }
+
+        private void GridButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button) sender;
+
+            if (button == null || Counter >= 9) return;
+            button.Content = Counter % 2 != 0 ? "X" : "O";
+            button.FontSize = 50;
+            button.Foreground = Brushes.Red;
+            Counter += 1;
+
+            if (CalculateVicotory())
+            {
+                var result = MessageBox.Show($"Player {button.Content} won!!", "Tic tac toe", MessageBoxButton.YesNo); ;
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        ResetGame();
+                        GameGrid.IsEnabled = true;
+                        break;
+                    case MessageBoxResult.No:
+                        ResetGame();
+                        GameGrid.IsEnabled = false;
+                        break;
+                }
+            }
+        }
+
+        private void NewGame_OnClick(object sender, RoutedEventArgs e)
+        {
+            ResetGame();
+            GameGrid.IsEnabled = true;
+        }
+
+        private void CloseGame_OnClick(object sender, RoutedEventArgs e)
+        {
+            ResetGame();
+            GameGrid.IsEnabled = false;
+        }
+
+        private void ResetGame()
+        {
+            ZeroZero.Content = null;
+            ZeroOne.Content = null;
+            ZeroTwo.Content = null;
+            OneZero.Content = null;
+            OneOne.Content = null;
+            OneTwo.Content = null;
+            TwoZero.Content = null;
+            TwoOne.Content = null;
+            TwoTwo.Content = null;
+
+            Counter = 0;
+        }
+
+        private bool CalculateVicotory()
+        {
+            if (Equals(ZeroZero.Content, ZeroOne.Content) && Equals(ZeroZero.Content, ZeroTwo.Content) && ZeroZero.Content != null)
+                return true;
+            if (Equals(ZeroZero.Content, OneOne.Content) && Equals(ZeroZero.Content, TwoTwo.Content) && ZeroZero.Content != null)
+                return true;
+            if (Equals(ZeroZero.Content, ZeroOne.Content) && Equals(ZeroZero.Content, ZeroTwo.Content) && ZeroZero.Content != null)
+                return true;
+            if (Equals(OneZero.Content, OneOne.Content) && Equals(OneZero.Content, OneTwo.Content) && OneZero.Content != null)
+                return true;
+            if (Equals(TwoZero.Content, TwoOne.Content) && Equals(TwoZero.Content, TwoTwo.Content) && TwoZero.Content != null)
+                return true;
+            if (Equals(ZeroOne.Content, OneOne.Content) && Equals(ZeroOne.Content, TwoOne.Content) && ZeroOne.Content != null)
+                return true;
+            if (Equals(ZeroTwo.Content, OneTwo.Content) && Equals(ZeroTwo.Content, TwoTwo.Content) && ZeroTwo.Content != null)
+                return true;
+            if (Equals(ZeroTwo.Content, OneOne.Content) && Equals(ZeroTwo.Content, TwoZero.Content) && ZeroTwo.Content != null)
+                return true;
+            return false;
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            //_clientSocket = new ClientSocket(InputIpAddress.Text, InputPortNumber.Text, out _contentLoaded);
+            _clientSocket = new ClientSocket("192.168.1.14", "8081", out _contentLoaded);
+
+            ConnectionText.Visibility = Visibility.Visible;
+            ConnectionText.Text = _contentLoaded ? "You are now connected to server" : "Connection to server failed";
+            if (_contentLoaded)
+            {
+                
+                GameMenuContainer.Visibility = Visibility.Visible;
+                ConnectContainer.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            _clientSocket.ClientSend(new Command { CommandTerm = "GETGAMES", Data = null });
+            DataContext = _clientSocket.ClientRecieveGames();
+        }
+
+        private void CreateNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            
+            _clientSocket.ClientSend(new Command { CommandTerm = "CREATE", Data = null });
+            _multiplayerGameId = _clientSocket.ClientRecieveGameId();
+
+            if (_multiplayerGameId != 0)
+            {
+                GameMenuContainer.Visibility = Visibility.Hidden;
+                GameGrid.Visibility = Visibility.Visible;
+                MultiplayerGameInfoContainer.Visibility = Visibility.Visible;
+                Client.Fill = Brushes.Red;
+                Opponent.Fill = Brushes.Blue;
+            }
+        }
+
+        private void JoinGame_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = GameListBox.SelectedItem;
+            if (selectedItem != null)
+            {
+                _clientSocket.ClientSend(new Command { CommandTerm = "JOIN", Data = GameListBox.SelectedItem });
+                var response = _clientSocket.JoinGameSucceeded();
+                if (response)
+                {
+                    GameGrid.Visibility = Visibility.Visible;
+                    GameMenuContainer.Visibility = Visibility.Hidden;
+                    MultiplayerGameInfoContainer.Visibility = Visibility.Visible;
+                    Client.Fill = Brushes.Blue;
+                    Opponent.Fill = Brushes.Red;
+                }
+            }
+        }
+
+        private void GameMode_Click(object sender, RoutedEventArgs e)
+        {
+            var content = (sender as Button)?.Content.ToString();
+            if (content == "Singleplayer")
+            {
+                GameGrid.Visibility = Visibility.Visible;
+                GameGrid.IsEnabled = true;
+                GameModeContainer.Visibility = Visibility.Hidden;
+                _gameMode = "Singleplayer";
+            }
+            else
+            {
+                ConnectContainer.Visibility = Visibility.Visible;
+                GameModeContainer.Visibility = Visibility.Hidden;
+                _gameMode = "Multiplayer";
+            }
+        }
+
+        private void LeaveGame_Click(object sender, RoutedEventArgs e)
+        {
+            GameMenuContainer.Visibility = Visibility.Visible;
+            GameGrid.Visibility = Visibility.Hidden;
+            MultiplayerGameInfoContainer.Visibility = Visibility.Hidden;
+            _clientSocket.ClientSend(new Command { CommandTerm = "LEAVE", Data = _multiplayerGameId});
+        }
+    }
+}
